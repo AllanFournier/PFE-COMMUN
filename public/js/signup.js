e2a8da61edd904b955ee7389061e5b2678d34878
Allan Fournier <allan.fournier.etu@univ-lille.fr>
2020-02-06 05:44:42 -0800

    handles the question

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,15 +6,43 @@ signBtn.addEventListener("click", e => {
   e.preventDefault();
   const email = signupForm["signup-email"].value;
   const password = signupForm["signup-password"].value;
+  const lastname = signupForm["signup-lastname"].value;
+  const firstname = signupForm["signup-firstname"].value;
 
-    if(email != null && password != null){
-        firebase.auth().createUserWithEmailAndPassword(email, password).then( cred => {
-            console.log(cred);
-            document.location.href = "/index.html";
-          }).catch(function(error) {
-            console.log(error);
-            // ...
+  if (email != null && password != null) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
+      console.log(cred);
+      query = firebase.firestore().collection("group").doc(group);
+
+      query.get().then(function (doc) {
+        if (doc.exists) {
+          firebase.firestore().collection("users").doc(cred.user.uid).set({
+            lastname: lastname,
+            firstname: firstname,
           });
-    }
-  
+          console.log("Document data:", doc.data());
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      }).catch(function (error) {
+        console.log("Error getting document:", error);
+      });
+      var user = firebase.auth().currentUser;
+      user.updateProfile({
+        displayName: firstname + " " +lastname,
+      }).then(function() {
+        // Update successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
+
+      
+      //
+    }).catch(function (error) {
+      console.log(error);
+      // ...
+    });
+  }
+
 })
